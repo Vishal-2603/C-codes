d23c6507e5c242ca1a0dd447031b9c85c03e0fe1
Vishal Pagere <vishalpagere2605@gmail.com>
2024-12-18 21:50:33 +0530

    /*prog to check area of rectangle*/

---
+++ Basic-Codes/areaofrectangle.c
@@ -0,0 +1,19 @@
+/*prog to check area of rectangle*/
+#include<stdio.h>
+int main()
+{
+   int l,b;
+  int area;
+ printf("enter length of rectangle");
+ scanf("%d",&l);
+ printf("enter breadth of rectangle");
+ scanf("%d",&b);
+  area=l*b;
+  printf("area of rectangle is %d");
+}
+
+/*
+enter length of rectangle 25
+enter breadth of rectangle 34
+area of rectangle is 6422292
+*/
