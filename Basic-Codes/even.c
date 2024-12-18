44ae8e77606665c69531bce95e92fbb5b747e44e
Vishal Pagere <vishalpagere2605@gmail.com>
2024-12-18 21:28:46 +0530

    Even odd program

---
+++ Basic-Codes/even.c
@@ -0,0 +1,17 @@
+/*prog to check a no is even or odd*/
+#include<stdio.h>
+void main()
+{
+    int num;
+   printf("enter the number");
+   scanf("%d",&num);
+   if(num%2==0) 
+   printf(" %d is even number",num);
+   else
+    printf(" %d is odd number",num);
+}
+
+/*
+enter the number12
+ 12 is even number
+*/
